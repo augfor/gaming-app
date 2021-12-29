ba81dd457ff1c6dac141ee9c5d642036f820ae22
Augusto Forero <a.forarc@gmail.com>
2021-12-29 08:16:13 -0500

    Add react-router-dom and Games page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import * as React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// Pages
 import Landing from './pages/Landing';
+import Games from './pages/Games';
 
 function App() {
   return (
     <>
-      <Landing></Landing>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/games" element={<Games />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Games.js
@@ -0,0 +1,3 @@
+export default function Games() {
+  return <h1>Hello World!</h1>;
+}
