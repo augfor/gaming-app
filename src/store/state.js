d98f91144154846151139128e8deab81a1c915e6
Augusto Forero <a.forarc@gmail.com>
2022-01-09 22:58:40 -0500

    Save and load state with localStorage

--- src/store/state.js
+++ src/store/state.js
@@ -1,8 +1,16 @@
-const initialState = {
+export const blankState = {
   user: {
     firstName: '',
     email: ''
   }
 };
+
+let initialState = blankState;
+
+const storedState = localStorage.getItem('state');
+
+if (storedState) {
+  initialState = JSON.parse(storedState);
+}
 
 export default initialState;
