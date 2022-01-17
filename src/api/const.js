308ac8dc9cf167480e98a154cc0aef90e19c0cb0
Augusto Forero <a.forarc@gmail.com>
2022-01-16 21:12:57 -0500

    Implement chat functionality

--- src/api/const.js
+++ src/api/const.js
@@ -2,4 +2,8 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const HEROKU_URL = process.env.REACT_APP_HEROKU_URL;
 export const RAWG_API_KEY = process.env.REACT_APP_RAWG_API_KEY;
+export const CHATENGINE_PROJECT_ID =
+  process.env.REACT_APP_CHATENGINE_PROJECT_ID;
+export const CHATENGINE_PRIVATE_KEY =
+  process.env.REACT_APP_CHATENGINE_PRIVATE_KEY;
 export const TOKEN_KEY = 'token';
--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,7 @@
 export const blankState = {
   user: {
     firstName: '',
+    lastName: '',
     email: ''
   }
 };
